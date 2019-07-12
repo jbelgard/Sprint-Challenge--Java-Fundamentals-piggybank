9746cbcbcf9cea10f65657e993d912c1e70d5006
Jason Belgard <belgard.jason@gmail.com>
2019-07-12 10:37:20 -0500

    Update Quarter.java

--- src/piggyBank/Quarter.java
+++ src/piggyBank/Quarter.java
@@ -1,6 +1,22 @@
 package piggyBank;
 
-public class Quarter
+public class Quarter extends Money
 {
-	
+	public Quarter(int count, int reducedCount)
+	{
+		super(count, reducedCount);
+		value = 0.25;
+	}
+
+	@Override
+	public String stringValue()
+	{
+		return "Number of Quarters - " + count;
+	}
+
+	@Override
+	public String toString()
+	{
+		return super.toString();
+	}
 }

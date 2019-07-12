2c77567c3d9b3df664b0dc4e4776dd830608fe1a
Jason Belgard <belgard.jason@gmail.com>
2019-07-12 10:30:55 -0500

    Update Dollar.java

---
+++ src/piggyBank/Dollar.java
@@ -0,0 +1,22 @@
+package piggyBank;
+
+public class Dollar extends Money
+{
+	public Dollar(int count, int reducedCount)
+	{
+		super(count, reducedCount);
+		value = 1.0;
+	}
+
+	@Override
+	public String stringValue()
+	{
+		return "Number of Dollars - " + count;
+	}
+
+	@Override
+	public String toString()
+	{
+		return super.toString();
+	}
+}
